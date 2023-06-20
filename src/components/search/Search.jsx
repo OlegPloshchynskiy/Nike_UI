09fba439a16519cf9b869435c3cfb46c952c1cbf
Olyezha <Oleg250901@gmail.com>
2023-06-20 06:43:57 +0300

    daaaa

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../button/Button";
 import search from "../../../public/images/search-icon.png";
 import { Link } from "react-router-dom";
 
 import style from "./search.module.css";
+import StoreInfo from "../../pages/storeInfo/StoreInfo";
 
 const Search = ({ func, btnFunc, nikeStores }) => {
+
+  const [storeState, setStoreState] = useState(0);
+  const [key, setKey] = useState(0);
+
+  const hendleStoreInfo = () => {
+    storeState === 1 ? setStoreState(0) : setStoreState(1) 
+    console.log(storeState);
+  }
+
   return (
     <div className={style.container}>
+
+      
+
       <p className={style.title}>Find a Nike Store</p>
       <div className={style.search_box}>
         <img src={search} alt="" />
@@ -23,7 +36,7 @@ const Search = ({ func, btnFunc, nikeStores }) => {
       <div className={style.filter}>
         {nikeStores.map((store, index) => {
           return (
-            <Link className={style.store_info_link} to="/storeinfo">
+            <Link className={style.store_info_link} to="/storeinfo" >
               <div className={style.card} key={index}>
                 <h2>{store.name}</h2>
                 <p>{store.vicinity}</p>
--- src/pages/storeInfo/StoreInfo.jsx
+++ src/pages/storeInfo/StoreInfo.jsx
@@ -1,17 +1,33 @@
-import React from 'react'
-import Layout from '../../layout/Layout'
-import { Link } from 'react-router-dom'
+import React from "react";
+import Layout from "../../layout/Layout";
+import { Link } from "react-router-dom";
 
-import style from './storeinfo.module.css'
+import Container from "../../components/container/Container";
 
-const StoreInfo = () => {
+import banner from "../../../public/images/store-banner.jpg";
+import store from "../../../public/images/classic-store.png";
+
+import style from "./storeinfo.module.css";
+
+const StoreInfo = ({ func }) => {
   return (
     <Layout>
-        <header className={style.header}>
-            <Link to="/findstore" className={style.go_back}>Back to Search</Link>
-        </header>
+      <header className={style.header}>
+        <Link className={style.go_back} to="/findstore">
+          Back to Search
+        </Link>
+      </header>
+      <Container>
+        <img src={banner} alt="" width="100%" />
+        <div className={style.store_info}></div>
+        <img src={store} alt="" width="80%" />
+        <p className={style.under_image}>
+          Gear up for sport with a wide selection of our newest footwear,
+          apparel and accessories. Shop Men`s, Women`s, <br /> Kids` and Jordan`
+        </p>
+      </Container>
     </Layout>
-  )
-}
+  );
+};
 
-export default StoreInfo
+export default StoreInfo;
